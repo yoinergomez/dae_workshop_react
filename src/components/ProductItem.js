d1603d6c74f10be5920261ea568d5d1fb1264373
yoinergomez <yoiner.gomez22@gmail.com>
2017-09-24 18:30:35 -0500

    Added style with masonry

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -5,8 +5,34 @@ export default class ProductItem extends Component {
 
     render() {
         var product = this.props.product
+        var image = product.thumbnail.replace("-I", "-O")
+
+        var style_h3 = {
+            color: 'blue',
+            'word-wrap': 'break-word',
+            width: '85%'
+        }
+
+        var style_div_item = {
+            margin: '10px 20px 20px 0',
+            transition: '0.3s',
+            'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',
+        }
+
+        var style_description = {
+            padding: '2px 16px',
+            width: '50%'
+        }
+
         return (
-                <img src={product.thumbnail} alt={product.title} />
+            <div style={style_div_item}>
+                <img src={image} alt={product.title} />
+              
+{/*                 <div style={style_description}>
+                    <h3 style={style_h3}>{product.title}</h3>
+                </div> */}
+                
+            </div>   
         );
     }
 
--- src/components/ProductsItemList.js
+++ src/components/ProductsItemList.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import ProductItem from './ProductItem.js'
+import MasonryLayout from 'react-masonry-component'
+
 
 export default class ProductsItemList extends Component {
 
+
     render() {
-        var resultItems = this.props.itemsData.map(function(product) {
+        var style = {
+            margin: '6em 3% 19em 3%'
+        };
+
+
+        var resultItems = this.props.itemsData.map(function (product) {
             return <ProductItem key={product.id} product={product} />
         });
-        return(
-            <div>
-                {resultItems}
-            </div>           
+        return (
+            <MasonryLayout
+                style={style}>
+                    { resultItems }
+            </MasonryLayout>           
         );
-    }
+}
 
 }
